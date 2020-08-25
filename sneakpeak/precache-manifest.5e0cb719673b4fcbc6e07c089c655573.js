abd6c56aedff541dc3ff02728a9755f69fdd6dd9
Luis Zaman <zamanlh@eeb-c02z20e0m0xv.attlocal.net>
2020-08-24 22:26:32 -0400

    update

--- sneakpeak/precache-manifest.5e0cb719673b4fcbc6e07c089c655573.js
+++ sneakpeak/precache-manifest.5e0cb719673b4fcbc6e07c089c655573.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "4f2fa7f0f06bcd988b7e88aab8b2042a",
-    "url": "/index.html"
+    "url": "index.html"
   },
   {
     "revision": "daed61760b24f019772b",
-    "url": "/static/js/2.920d518f.chunk.js"
+    "url": "static/js/2.920d518f.chunk.js"
   },
   {
     "revision": "b1fb7d34775d0e08f78e5a934073168f",
-    "url": "/static/js/2.920d518f.chunk.js.LICENSE.txt"
+    "url": "static/js/2.920d518f.chunk.js.LICENSE.txt"
   },
   {
     "revision": "868b9f09b24ec2c402b1",
-    "url": "/static/js/main.757493f5.chunk.js"
+    "url": "static/js/main.757493f5.chunk.js"
   },
   {
     "revision": "7b7a1df8c95ea6480696",
-    "url": "/static/js/runtime-main.4bb3a5e6.js"
+    "url": "static/js/runtime-main.4bb3a5e6.js"
   }
 ]);
