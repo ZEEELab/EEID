ce7449ce05e337836fd72e4b826ecd03fb127c30
Luis Zaman <luis.zaman@gmail.com>
2021-09-04 22:49:38 -0400

    add side by side

---
+++ sidebyside/precache-manifest.73df33b42111fa4d36f9de038a710024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2514cbb11dba8e5bf7c364582e8de256",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cba80be3b3aad8666c33",
+    "url": "./static/js/2.2d8856d9.chunk.js"
+  },
+  {
+    "revision": "b1fb7d34775d0e08f78e5a934073168f",
+    "url": "./static/js/2.2d8856d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f12932692bfed7e4581",
+    "url": "./static/js/main.f900f562.chunk.js"
+  },
+  {
+    "revision": "44f57af82892c1eaaca7",
+    "url": "./static/js/runtime-main.efaed524.js"
+  }
+]);
